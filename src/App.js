e632a2ea3a24221a73d87ad0f5ef9da07063791d
vancouver29 <vancouver2990@gmail.com>
2020-06-17 20:49:16 +0200

    upload Todolist project

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import Header from "./components/layout/Header";
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
+
+import "./App.css";
+
+class App extends React.Component {
+    state = {
+        todos: [{
+                id: 1,
+                title: "Take out the trash",
+                completed: false,
+            },
+            {
+                id: 2,
+                title: "Dinner with wife",
+                completed: true,
+            },
+            {
+                id: 3,
+                title: "Meeting up with boss",
+                completed: false,
+            },
+        ],
+    };
+
+    // Toggle Complete
+    markComplete = (id) => {
+        this.setState({
+            todos: this.state.todos.map((todo) => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed;
+                }
+                return todo;
+            }),
+        });
+    };
+
+    // Delete Todo
+    delTodo = (id) => {
+        this.setState({
+            todos: [...this.state.todos.filter((todo) => todo.id !== id)],
+        });
+    };
+
+    // Add Todo
+    addTodo = (title) => {
+        console.log(title);
+    };
+
+    render() {
+        return ( <
+            div className = "App" >
+            <
+            div className = "container" >
+            <
+            Header / >
+            <
+            AddTodo addTodo = { this.addTodo }
+            /> <
+            Todos todos = { this.state.todos }
+            markComplete = { this.markComplete }
+            delTodo = { this.delTodo }
+            /> <
+            /div> <
+            /div>
+        );
+    }
+}
+
+export default App;
